53def8d79aacbcab3d072634c8678b712f6c94a7
Vedavathi-S <vedavathivedavathi18@gmail.com>
2026-09-02 23:10:05 +0530

    Added login and register pages

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+
+      <div>
+        <h3>Total Requests</h3>
+        <p>8</p>
+      </div>
+
+      <div>
+        <h3>Open</h3>
+        <p>3</p>
+      </div>
+
+      <div>
+        <h3>In Progress</h3>
+        <p>2</p>
+      </div>
+
+      <div>
+        <h3>Resolved</h3>
+        <p>3</p>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
